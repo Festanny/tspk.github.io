8f08410ef9215be0783e0fa62f74b18906b57415
Festanny <junior-format30@mail.ru>
2021-12-17 21:00:58 +0400

    body overflow

--- js/script.js
+++ js/script.js
@@ -9,23 +9,34 @@ $(document).on('ready', function() {
 
 
 // Сдвиг label
-let inputStart = document.querySelectorAll('.auth .form_block .inputDefault');
-let labelFocus = document.querySelectorAll('.auth .form_block #labelFocus');
+var inputStart = document.querySelectorAll('.auth .form_block .inputDefault'),
+    labelFocus = document.querySelectorAll('.auth .form_block #labelFocus');
 
 inputStart[0].onfocus = function() {
 	labelFocus[0].classList = "labelFocus";
-}
+};
 inputStart[0].onblur = function() {
     if (inputStart[0].value == "") {
         labelFocus[0].classList = "inputV";
     }
-}
-
+};
 inputStart[1].onfocus = function() {
 	labelFocus[1].classList = "labelFocus";
-}
+};
 inputStart[1].onblur = function() {
     if (inputStart[0].value == "") {
         labelFocus[1].classList = "inputV";
+    }
+};
+
+// Выключение overflow у body
+let ovHidden = document.querySelector('.nav__trigger');
+
+ovHidden.onclick = () => {
+    if (ovHidden.className == 'nav__trigger is-active') {
+        document.body.style.overflowY = 'hidden';
+    }
+    else {
+        document.body.style.overflowY = 'scroll';
     }
 }
