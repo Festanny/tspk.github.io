214595b8753470e4a1b1e18c060d97c0c5366d69
Festanny <60522731+Festanny@users.noreply.github.com>
2021-12-25 11:34:26 +0400

    new all news

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ inputStart[1].onblur = function() {
     }
 };
 
-// Выключение overflow у body
+// Выключение overflow у body при открытом меню
 var ovHidden = document.querySelector('.nav__trigger');
 
 ovHidden.onclick = () => {
@@ -40,3 +40,82 @@ ovHidden.onclick = () => {
         document.body.style.overflowY = 'scroll';
     }
 };
+
+// Светлая/темная тема
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+// Предварительный загрузчик
+window.onload = function() {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+    }, 500);
+    document.body.style.overflow = 'auto';
+}
+
+// Кнопка наверх
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        if ($('#upbutton').is(':hidden')) {
+            $('#upbutton').css({
+                opacity: 1
+            }).fadeIn('slow');
+        }
+    } else {
+        $('#upbutton').stop(true, false).fadeOut('fast');
+    }
+});
+$('#upbutton').click(function() {
+    $('html, body').stop().animate({
+        scrollTop: 0
+    }, 300);
+});
+
+// Прогресс
+$(function() {
+    $(window).on("scroll resize", function() {
+        var o = $(window).scrollTop() / ($(document).height() - $(window).height());
+        $(".progress-bar").css({
+            "width": (100 * o | 0) + "%"
+        });
+        $('progress')[0].value = o;
+    })
+});
+
+// Появление дополнительного контента из блока
+var acc = document.getElementsByClassName("learn-more");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
